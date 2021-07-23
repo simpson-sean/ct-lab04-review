08df9e25003c9d76d36f11b93ec51c21b6675835
Sean <simpson.sean@outlook.com>
2021-07-23 14:08:03 -0700

    WORKDONE: First Passing test on first Model: Birds

--- __tests__/bird.test.js
+++ __tests__/bird.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import bird_model from '../lib/models/bird-model.js';
 
 // CRUD
 // C - create  POST   --> INSERT
@@ -9,10 +10,22 @@ import app from '../lib/app.js';
 // U - update  PUT    --> UPDATE
 // D - delete  DELETE --> DELETE
 
-describe('demo routes', () => {
+describe('bird routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
+  it('creates a bird', async () => {
+    const polly = { name: 'polly', breed: 'mccaw', age: 15 };
+    const res = await request(app).post('/api/v1/birds').send(polly);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...polly,
+
+    });
   
-});
+  });
+
+
+}) //<----END OF PARENT CODE BLOCK
